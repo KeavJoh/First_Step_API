a2c5d82e3eb663df4757f1c22c629b301f4d1a47
Kevin Johr <kevin.johr@posteo.de>
2023-06-21 11:46:09 +0200

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace First_Step_API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
