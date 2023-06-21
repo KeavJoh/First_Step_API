12d9d45268aae34e4393261c65ae7fdc44752483
Kevin Johr <kevin.johr@posteo.de>
2023-06-21 21:58:12 +0200

    add saving a new job in the database

--- Controllers/JobPostingController.cs
+++ Controllers/JobPostingController.cs
@@ -1,10 +1,18 @@
-﻿using First_Step_API.Models;
+﻿using First_Step_API.Data;
+using First_Step_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace First_Step_API.Controllers
 {
     public class JobPostingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -27,6 +35,35 @@ namespace First_Step_API.Controllers
                     jobPosting.CompanyImage = bytes;
                 }
             }
+
+            if (jobPosting.Id == 0)
+            {
+                // add new job if not editing
+                _context.JobPostings.Add(jobPosting);
+            } else
+            {
+                // edit exists job
+                var jobFromDb = _context.JobPostings.FirstOrDefault(x => x.Id == jobPosting.Id);
+                //_context.JobPostings.Update(jobPosting);
+
+                if (jobFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                jobFromDb.StartDate = jobPosting.StartDate;
+                jobFromDb.Salary = jobPosting.Salary;
+                jobFromDb.ContactPhone = jobPosting.ContactPhone;
+                jobFromDb.ContactMail = jobPosting.ContactMail;
+                jobFromDb.ContactWebsite = jobPosting.ContactWebsite;
+                jobFromDb.JobTitle = jobPosting.JobTitle;
+                jobFromDb.CompanyImage = jobPosting.CompanyImage;
+                jobFromDb.CompanyName = jobPosting.CompanyName;
+                jobFromDb.JobDescription = jobPosting.JobDescription;
+                jobFromDb.JobLocation = jobPosting.JobLocation;
+            }
+
+            _context.SaveChanges();
 
             // write jobposting to db
 
